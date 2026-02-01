bcd77dd7005f9f9a6be4130d73fcfcff9b20c58d
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-01 12:03:21 +0530

    Encapsulation

---
+++ OOPs/OOPs-Encapsulation.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class SportsCar{
+    //Private prevent access from outside of the class or only accessable to class's method.
+    private:
+    string brand;
+    string model;
+    bool isEngineOn;
+    int speed;
+    int gear;
+
+    public:
+    SportsCar(string brand,string model){
+        cout<<"Sports car initiated";
+        this->brand=brand;
+        this->model=model;
+        this->isEngineOn=false;
+        this->speed = 0;
+        this->gear=0;
+    }
+
+    void startEngine(){
+        this->isEngineOn=true;
+        cout<<this->brand<<" "<<this->model<<" "<<"Engine started"<<endl;
+    }
+    void accelerate(){
+        if(!isEngineOn){
+            cout<<"Car is not started"<<endl;
+            return;
+        }
+        this->speed+=20;
+        cout<<this->brand<<" "<<this->model<<" accelerated at speed of "<<this->speed<<" km/h"<<endl;
+    }
+
+    void shiftGear(int gear){
+        if(!isEngineOn){
+            cout<<"Car is not started"<<endl;
+            return;
+        }
+        this->gear = gear;
+         cout<<this->brand<<" "<<this->model<<" "<<"Gear shifted to:  "<<this->gear<<endl;
+    }
+
+    void brake(){
+        this->speed-=20;
+        if(this->speed<0)this->speed = 0;
+        cout<<this->brand<<" "<<this->model<<" braked applied and now speed becomes "<<this->speed<<" km/h"<<endl;
+    }
+
+    void stopEngine(){
+        isEngineOn=false;
+        this->speed =0;
+        this->gear=0;
+        cout<<this->brand<<" "<<this->model<<" "<<"Engine stoped"<<endl;
+    }
+
+    ~SportsCar(){
+        cout<<"Sports car destructor is called"<<endl;
+    }
+
+};
+
+int main(){
+    SportsCar* mycar = new SportsCar("Tata","Sierra");
+    mycar->startEngine();
+    mycar->accelerate();
+    mycar->shiftGear(4);
+    mycar->brake();
+    mycar->stopEngine();
+    delete mycar;
+    return 0;
+}
