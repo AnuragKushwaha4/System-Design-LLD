73972eb93aa7e7e552fa6f61517c5b7acf65137e
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-05 21:57:55 +0530

    Starting with OCP

---
+++ SOLID DESIGN PRINCIPLES/OpenCloseP.cpp
@@ -0,0 +1,12 @@
+/*Open Close Principle states that a class should me allowed to
+ extend but not to modifiy means if user want ot add new functionality it must not change existing
+ class... */
+
+ /* In example of Single Responsibity Principle's example we worked on a problem now in same problem
+ if user want to add two new functionality in save to DB class which are Save to SQL and Save to File
+  directly adding in Save to DB class will break openClose functionality..*/
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
