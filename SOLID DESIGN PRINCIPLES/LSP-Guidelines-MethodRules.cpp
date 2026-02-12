bc50fe3c3bb275bd263747adc9da5d0dab218596
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-12 22:59:06 +0530

    LSP Method rule istated

---
+++ SOLID DESIGN PRINCIPLES/LSP-Guidelines-MethodRules.cpp
@@ -0,0 +1,14 @@
+/*
+Method rules are the Guideline to ensure LSP properly implemented as a name suggest that it is
+ related to methods of a class--> it is basically a condition that a method must 
+ satisfy.... it is of tow types:
+PreConditioned and postCondition.
+
+Precondition --- In this a method should follow a specific condition before implementations so its 
+child that overriding that functionality must also follow that same condition or must follow more 
+strengthern condition...
+PostCondition--- In this a method should follow a specific condition after implementations so its 
+child that overriding that functionality must also follow that same condition or must follow more 
+weaker condition...
+
+*/
