d1ada08ca7af1de830c4dd4103672a7fe1927a13
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-02-16 22:44:40 +0530

    Completed with Bad Designing of Google docs

--- GOOGLE DOCS MANAGER/BAD DESIGN/BadDesign.cpp
+++ GOOGLE DOCS MANAGER/BAD DESIGN/BadDesign.cpp
@@ -1,8 +1,45 @@
 #include<iostream>
+#include <vector>
+#include<string>
+
+
 using namespace std;
 class DocumentManager{
+    vector<string> documents;
 
+    public:
+    void addImage(string path){
+        documents.push_back(path);
+    }
+
+    void addText(string text){
+        documents.push_back(text);
+    }
+
+    string renderDocument(){
+        string results;
+
+        for(auto element : documents){
+            if(element.length()>4 && element.substr(element.length()-4)==".jpg" || element.substr(element.length()-4)==".png" ){
+                results+="[Image path: "+element + "] \n";
+            }
+            else
+                results+=element+ " \n";
+        }
+
+        return results;
+
+    }
 };
 int main(){
+
+    DocumentManager* D1= new DocumentManager();
+    D1->addImage("Image1.jpg");
+    D1->addText("Hello guys how are you");
+    D1->addText("This is Anurag Learning LLD");
+    D1->addImage("Image2.jpg");
+    D1->addText("India won against pakistan");
+    D1->addImage("Image3.jpg");
+    cout<<D1->renderDocument();
     return 0;
 }
